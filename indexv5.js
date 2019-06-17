02f1d36c9295e8f6521d9f4d93beaaf8a119a3a2
Martin Schray <mschray@microsoft.com>
2019-06-17 10:52:07 -0500

    Add msg b4 displaying the text found in the image.

--- indexv5.js
+++ indexv5.js
@@ -4,5 +4,6 @@ const result = require('dotenv').config();
 const { recognizeText, getAllText } = require("./CognitiveServicesOCR");
 
 recognizeText("./parking.jpg").then(function (data) {
+    console.log("Text returned from the image is:\n");
     console.log(getAllText(data));
 });
